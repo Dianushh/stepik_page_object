b8c29ed63ad7c12e60e6a5a217d5aef82b0d6d61
Dianochka <stoljarova2003@yandex.ru>
2025-10-10 17:11:30 +0300

    Добавили исключение ошибки

--- test_main_page.py
+++ test_main_page.py
@@ -16,3 +16,9 @@ def test_guest_can_go_to_login_page(browser):
     
     # Можно добавить проверку, что страница загрузилась
     assert "login" in browser.current_url
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
