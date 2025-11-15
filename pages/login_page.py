ddb2f6bd96bd8c13f9cc616ee2724b7e696de6f3
Dianochka <stoljarova2003@yandex.ru>
2025-11-15 14:11:19 +0300

    https://github.com/Dianushh/stepik_page_object.git

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators  # ← ДОБАВЬТЕ ИМПОРТ!
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert LoginPageLocators.LOGIN_URL in current_url, \
+            f"Expected '{LoginPageLocators.LOGIN_URL}' to be in current url '{current_url}'"  
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,32 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    
-    # Проверяем, что перешли на страницу логина
-    WebDriverWait(browser, 10).until(
-        EC.url_contains("login")
-    )
-    
-    # Можно добавить проверку, что страница загрузилась
-    assert "login" in browser.current_url
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+    # Переходим на страницу логина и получаем объект LoginPage
+    login_page = page.go_to_login_page()
+    
+    # Проверяем, что это действительно страница логина
+    login_page.should_be_login_page()
+
+def test_guest_should_see_login_url(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
